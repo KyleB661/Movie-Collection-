5e42fdd36334fddad6cbbf921583a45ef635a1b4
kyleb661 <kylebell92@msn.com>
2025-02-11 19:58:58 +0000

    Completed CRUD functionality and started to style

---
+++ controllers/user.js
@@ -0,0 +1,95 @@
+const express = require("express");
+const router = express.Router();
+const User = require("../models/user");
+
+router.get("/:id", async (req, res) => {
+    const userId = req.params.id;
+    const user = await User.findById(userId);
+  
+    res.render("user/home.ejs", {user})
+  })
+  
+  router.get("/:id/movie/new", async (req, res) => {
+    const userId = req.params.id;
+  
+    res.render("movie/new.ejs", {userId})
+  })
+  
+  router.post("/:id/movie", async (req, res) => {
+    const userId = req.params.id;
+    const { title, genre, runtime, director, yearOfRelease } = req.body;
+  
+    const user = await User.findById(userId);
+    user.movies.push({ title, genre, runtime, director, yearOfRelease });
+  
+    await user.save();
+  
+    res.redirect(`/users/${userId}`);
+  });
+
+  router.get("/:userId/movie/:movieId", async (req, res) => {
+    try {
+      const { userId, movieId } = req.params;
+  
+      const user = await User.findById(userId);
+      if (!user) {
+        return res.redirect("/error");
+      }
+  
+      const movie = user.movies.id(movieId);
+      if (!movie) {
+        return res.redirect("/error");
+      }
+  
+      res.render("movie/show.ejs", { movie, user });
+    } catch (err) {
+      console.error(err);
+      res.redirect("/error");
+    }
+  });
+  
+  router.get("/:userId/movie/:movieId/edit", async (req, res) => {
+    const { userId, movieId } = req.params;
+  
+    try {
+      const user = await User.findById(userId);
+      const movie = user.movies.id(movieId);
+      res.render("movie/edit.ejs", { movie, user });
+    } catch (err) {
+      console.error(err);
+      res.redirect("/error");
+    }
+  });
+  
+  router.put("/:id/movie/:movieId", async (req, res) => {
+    const userId = req.params.id;
+    const movieId = req.params.movieId;
+    const { title, genre, runtime, director, yearOfRelease } = req.body;
+  
+    const user = await User.findById(userId);
+    const movie = user.movies.id(movieId);
+  
+    movie.title = title;
+    movie.genre = genre;
+    movie.runtime = runtime;
+    movie.director = director;
+    movie.yearOfRelease = yearOfRelease;
+  
+    await user.save();
+  
+    res.redirect(`/users/${userId}`);
+  });
+  
+  router.delete("/:id/movie/:movieId", async (req, res) => {
+    const userId = req.params.id;
+    const movieId = req.params.movieId;
+  
+    const user = await User.findById(userId);
+    user.movies.pull({ _id: movieId });  
+  
+    await user.save();
+  
+    res.redirect(`/users/${userId}`);
+  });
+  
+module.exports = router;
--- server.js
+++ server.js
@@ -9,8 +9,6 @@ const morgan = require("morgan");
 const session = require("express-session");
 
 const port = process.env.PORT ? process.env.PORT : "3000";
-const authController = require("./controllers/auth.js");
-const User = require("./models/user.js");
 
 mongoose.connect(process.env.MONGODB_URI);
 
@@ -40,87 +38,14 @@ app.get("/error", (req, res) => {
   res.render("error.ejs")
 });
 
+const authController = require("./controllers/auth.js");
 app.use("/auth", authController);
 
-app.get("/users/:id", async (req, res) => {
-  const userId = req.params.id;
-  const user = await User.findById(userId);
+const isSignedIn = require("./middleware/is-signed-in");
+app.use(isSignedIn);
 
-  res.render("user/home.ejs", {user})
-})
-
-app.get("/users/:id/movie/new", async (req, res) => {
-  const userId = req.params.id;
-
-  res.render("movie/new.ejs", {userId})
-})
-
-app.post("/users/:id/movie", async (req, res) => {
-  const userId = req.params.id;
-  const { title, genre, runtime, director, yearOfRelease } = req.body;
-
-  const user = await User.findById(userId);
-  user.movies.push({ title, genre, runtime, director, yearOfRelease });
-
-  await user.save();
-
-  res.redirect(`/users/${userId}`);
-});
-
-app.get("/movies/:movieId", async (req, res) => {
-  const movieId = req.params.movieId;
-  const userId = req.session.user._id;
-  
-  const user = await User.findById(userId);
-  const movie = user.movies.id(movieId);
-  
-  res.render("movie/show.ejs", { movie, user });
-});
-
-app.get("/users/:userId/movie/:movieId/edit", async (req, res) => {
-  const { userId, movieId } = req.params;
-
-  try {
-    const user = await User.findById(userId);
-    const movie = user.movies.id(movieId);
-    res.render("movie/edit.ejs", { movie, user });
-  } catch (err) {
-    console.error(err);
-    res.redirect("/error");
-  }
-});
-
-app.put("/users/:id/movie/:movieId", async (req, res) => {
-  const userId = req.params.id;
-  const movieId = req.params.movieId;
-  const { title, genre, runtime, director, yearOfRelease } = req.body;
-
-  const user = await User.findById(userId);
-  const movie = user.movies.id(movieId);
-
-  movie.title = title;
-  movie.genre = genre;
-  movie.runtime = runtime;
-  movie.director = director;
-  movie.yearOfRelease = yearOfRelease;
-
-  await user.save();
-
-  res.redirect(`/users/${userId}`);
-});
-
-app.delete("/users/:id/movie/:movieid", async (req, res) => {
-  const userId = req.params.id;
-  const movieId = req.params.movieid;
-
-  const user = await User.findById(userId);
-  user.movies.pull({ _id: movieId });  
-  
-  await user.save();
-
-  res.redirect(`/users/${userId}`);
-});
-
+const userController = require("./controllers/user");
+app.use("/users",  userController);
 
 app.listen(port, () => {
   console.log(`The express app is ready on port ${port}!`);
